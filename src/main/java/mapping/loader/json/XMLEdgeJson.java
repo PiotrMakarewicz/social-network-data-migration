d2b7a1e924869f6df92381ee435b442d2c73d88b
PiotrMakarewicz <makarepio@gmail.com>
2022-07-30 19:37:37 +0200

    adjust xml mapping schema

--- src/main/java/mapping/loader/json/XMLEdgeJson.java
+++ src/main/java/mapping/loader/json/XMLEdgeJson.java
@@ -2,21 +2,13 @@ package mapping.loader.json;
 
 import java.util.Map;
 
-/**
- Zakładamy możliwość łączenia w węzły tagów:
- - rodzic-dziecko, np. /dblp/article z /dblp/article/author
- - dziecko-dziecko, np. /dblp/article/author z /dblp/article/author, jeżeli oba te tagi mają wspólnego rodzica
- W obu przypadkach:
- "mappedAttributes" - mapowania z atrybutów rodzica (w przypadku relacji dziecko-dziecko: z tagu zewnętrznego wobec obu dzieci)
- "mappedChildElements" - mapowania z dzieci rodzica. Mapowany jest tekst między tagiem otwierającym i zamykającym dziecka.
- */
+
 public class XMLEdgeJson {
-    private String edgeLabel;
-    private boolean directed;
+    protected String edgeLabel;
+    protected boolean edgeType;
 
-    private String firstNodeMappingId;
-    private String secondNodeMappingId;
+    protected String childNodeLabel;
 
-    private Map<String, String> mappedAttributes;
-    private Map<String, String> mappedChildElements;
+    protected Map<String, String> mappedParentAttributes;
+    protected Map<String, String> mappedParentsChildElementInnerTexts;
 }
--- src/main/java/mapping/loader/json/XMLNodeJson.java
+++ src/main/java/mapping/loader/json/XMLNodeJson.java
@@ -4,11 +4,10 @@ import java.util.Map;
 
 
 public class XMLNodeJson {
-    private String nodeMappingId;
     private String nodeLabel;
     private String xpath; // np. "/dblp/article/author"
     private String innerTextMappedTo; // jeśli chcemy zmapować np. <author>Jan Kowalski</author> na jakiś atrybut węzła w grafie
 
     private Map<String, String> mappedAttributes; // mapowanie atrybutów tagu
-    private Map<String, String> mappedChildElements; // mapowany jest tekst między tagiem otwierającym i zamykającym
+    private Map<String, String> mappedChildElementInnerTexts; // mapowany jest tekst między tagiem otwierającym i zamykającym
 }
