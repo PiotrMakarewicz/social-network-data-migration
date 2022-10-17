334ea9e6011dc2045216746fa0cda51551bd982f
PiotrMakarewicz <makarepio@gmail.com>
2022-10-17 12:02:35 +0200

    Setup server

--- social-network-data-migration-server/src/main/java/pl/edu/agh/socialnetworkdatamigration/SocialNetworkDataMigrationServer.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.agh.socialnetworkdatamigration;
-
-public class SocialNetworkDataMigrationServer {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
---
+++ social-network-data-migration-server/src/test/java/pl/edu/agh/socialnetworkdatamigration/server/SocialNetworkDataMigrationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.socialnetworkdatamigration.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkDataMigrationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
