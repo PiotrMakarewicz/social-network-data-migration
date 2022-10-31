6a5d292db38a8cacbf5502d4b24a3e3efb062654
PiotrMakarewicz <makarepio@gmail.com>
2022-10-31 10:10:27 +0100

    Separate file loading from mapping deserialization

--- social-network-data-migration-core/src/main/java/pl/edu/agh/socialnetworkdatamigration/core/mapping/loader/CSVMappingLoader.java
+++ social-network-data-migration-core/src/main/java/pl/edu/agh/socialnetworkdatamigration/core/mapping/loader/CSVMappingLoader.java
@@ -3,12 +3,9 @@ package pl.edu.agh.socialnetworkdatamigration.core.mapping.loader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import pl.edu.agh.socialnetworkdatamigration.core.mapping.loader.json.CSVMappedColumnsDeserializer;
 import pl.edu.agh.socialnetworkdatamigration.core.mapping.CSVSchemaMapping;
+import pl.edu.agh.socialnetworkdatamigration.core.mapping.loader.json.CSVMappedColumnsDeserializer;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
@@ -26,20 +23,13 @@ public class CSVMappingLoader implements MappingLoader<CSVSchemaMapping> {
         this.headers = getHeaders(csvInputPath, fieldTerminator);
     }
 
-    public CSVSchemaMapping load(String configPath) {
+    public CSVSchemaMapping loadFromJson(String jsonStr) {
         Type t = new TypeToken<Map<Integer, String>>(){}.getType();
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(t, new CSVMappedColumnsDeserializer(this.withHeaders, this.headers))
                 .create();
 
-        JsonReader reader;
-        try {
-            reader = new JsonReader(new FileReader(configPath));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        CSVSchemaMapping schemaMapping = gson.fromJson(reader, CSVSchemaMapping.class);
+        CSVSchemaMapping schemaMapping = gson.fromJson(jsonStr, CSVSchemaMapping.class);
         List.of(schemaMapping.getFromNodeMapping(), schemaMapping.getToNodeMapping()).forEach(
                 nm -> {
                     if (nm.getNodeLabel() == null || nm.getMappedColumns().isEmpty())
--- social-network-data-migration-core/src/test/java/pl/edu/agh/socialnetworkdatamigration/core/mapping/loader/CSVMappingLoaderTest.java
+++ social-network-data-migration-core/src/test/java/pl/edu/agh/socialnetworkdatamigration/core/mapping/loader/CSVMappingLoaderTest.java
@@ -3,21 +3,28 @@ package pl.edu.agh.socialnetworkdatamigration.core.mapping.loader;
 import pl.edu.agh.socialnetworkdatamigration.core.mapping.CSVSchemaMapping;
 import pl.edu.agh.socialnetworkdatamigration.core.mapping.edge.CSVEdgeMapping;
 import org.junit.jupiter.api.Test;
-import pl.edu.agh.socialnetworkdatamigration.core.mapping.loader.CSVMappingLoader;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CSVMappingLoaderTest {
     String csvInputPath = getClass().getClassLoader().getResource("test.csv").getPath();
     String csvWithHeadersMappingsPath = getClass().getClassLoader().getResource("csv_with_headers.json").getPath();
+    String csvWithHeadersMappingsJson = Files.readString(Path.of(csvWithHeadersMappingsPath));
     String csvNoHeadersMappingsPath = getClass().getClassLoader().getResource("csv_no_headers.json").getPath();
+
+    String csvNoHeadersMappingsJson = Files.readString(Path.of(csvNoHeadersMappingsPath));
+
+    public CSVMappingLoaderTest() throws IOException {}
 
     @Test
     public void testWithHeaders() throws FileNotFoundException {
         var loader = new CSVMappingLoader(csvInputPath, true);
-        CSVSchemaMapping mapping = loader.load(csvWithHeadersMappingsPath);
+        CSVSchemaMapping mapping = loader.loadFromJson(csvWithHeadersMappingsJson);
         var from = mapping.getFromNodeMapping();
         var to = mapping.getToNodeMapping();
         var edge = mapping.getEdgeMapping();
@@ -41,7 +48,7 @@ public class CSVMappingLoaderTest {
     @Test
     public void testNoHeaders() throws FileNotFoundException {
         var loader = new CSVMappingLoader(csvInputPath, false);
-        CSVSchemaMapping mapping = loader.load(csvNoHeadersMappingsPath);
+        CSVSchemaMapping mapping = loader.loadFromJson(csvNoHeadersMappingsJson);
         var from = mapping.getFromNodeMapping();
         var to = mapping.getToNodeMapping();
         var edge = mapping.getEdgeMapping();
